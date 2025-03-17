0bf930014691e31d00eca17f90763c64970fbac7
tymofiichuy <tymofiichuy@gmail.com>
2025-03-17 11:39:45 +0200

    Update structure

--- src/factorization.hpp
+++ src/factorization.hpp
@@ -10,6 +10,12 @@ public:
     virtual ~primality_test() = default;
 };
 
+class solovay_strassen final:public primality_test{
+private:
+public:
+    bool test(uint64_t in, const int& precision) override;
+};
+
 class factorization_algorithm{
 public:
     virtual uint64_t factor(uint64_t in) = 0;
